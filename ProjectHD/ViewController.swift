974a7072b9e03992f17f28ece756cdc942c50217
Stephen Muscarella <stephen.m@msistone.com>
2018-06-13 12:19:47 -0700

    Init

---
+++ ProjectHD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectHD
+//
+//  Created by Stephen Muscarella on 6/13/18.
+//  Copyright © 2018 Elite Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
